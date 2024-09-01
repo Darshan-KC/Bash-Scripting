5a221f4f861ea18959490993a9c184e8c4b03352
Darshan KC <darshanbahadurchhetri@gmail.com>
2024-09-01 20:52:58 +0545

    system veriable

---
+++ system-variable.sh
@@ -0,0 +1,15 @@
+# Let's learn some system defined variables
+
+#!/bin/bash
+
+echo $HOME # Home Directory
+
+echo $PWD # Present working directory
+
+echo $BASH # Bash Shell name 
+
+echo $BASH_VERSION # Bash Shell version
+
+echo $LOGNAME # Name of Login User
+
+echo $OSTYPE # Type of OS
