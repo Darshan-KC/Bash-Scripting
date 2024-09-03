556226d4cebf311802c09e9c68ed0001811133bd
Darshan KC <darshanbahadurchhetri@gmail.com>
2024-09-03 20:40:03 +0545

    date update

--- date.sh
+++ date.sh
@@ -5,3 +5,11 @@
 d=$(date +%Y-%m-%d)
 echo "Date format in YYYY-MM-DD"
 echo $d
+
+d=$(date +%a)
+echo "Name of the week day in short form"
+echo $d
+
+d=$(date +%A)
+echo "Name of the week day in full form"
+echo $d
