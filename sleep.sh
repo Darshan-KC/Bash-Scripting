cd403977bcc048f2bbf5d6e445a82bd59ce6d31e
Darshan KC <darshanbahadurchhetri@gmail.com>
2024-09-08 22:53:42 +0545

    sleep done

--- sleep.sh
+++ sleep.sh
@@ -1,3 +1,17 @@
 # Let's learn how to use sleep
 
 #! /bin/bash
+
+date +"%H:%M:%S"
+
+echo "Wait for 3 seconds"
+
+# sleep for 3 seconds
+
+sleep 3
+
+# you can use 3s also
+
+date +"%H:%M:%S"
+
+echo "Task Completed"
