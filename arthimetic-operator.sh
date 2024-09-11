a62919d4299648e8c690111667f6d88cd97fb84a
Darshan KC <darshanbahadurchhetri@gmail.com>
2024-09-11 20:18:03 +0545

    arithmetic operation complete

--- arthimetic-operator.sh
+++ arthimetic-operator.sh
@@ -11,3 +11,16 @@ echo "Difference = $sub"
 
 mul=$((a * b))
 echo "Multiplication = $mul"
+
+div=$((a / b))
+echo "Division = $div"
+
+exp=$((a ** b))
+echo "Exponential = $exp"
+
+echo "Increment = $((a++))"
+echo "Now a is $a"
+
+let "a %= 2"
+echo "Remainder of Dividing a by 2, a = "
+echo $a    
