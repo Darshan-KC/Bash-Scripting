bdbac9ac5e4ca2a13d68f2164b8dcd03cfd5051d
Darshan KC <darshanbahadurchhetri@gmail.com>
2024-09-22 22:53:15 +0545

    case running

--- case.sh
+++ case.sh
@@ -1,3 +1,10 @@
 # The Bash case statement is the simplest form of IF-THEN-ELSE with many ELIF elements
 
 #! /bin/bash
+
+echo "Do you know Java programming ?"
+read -p "Yes/No? :" Answer
+
+case $Answer in
+ Yes|yes|Y|y)
+ 
