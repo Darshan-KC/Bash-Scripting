4db35f136592f13c3d66871d4cc12d48e597efb7
Darshan KC <darshanbahadurchhetri@gmail.com>
2024-10-08 21:49:11 +0545

    until loop multi condition

---
+++ until-loop-multi-condition.sh
@@ -0,0 +1,14 @@
+#Bash Until Loop example with multiple conditions 
+
+#! /bin/bash
+
+max=5
+a=1
+b=0
+
+until [[ $a -gt $max || $b -gt $max ]];
+do
+    echo "a = $a & b = $b."
+    ((a++))
+    ((b++))
+done
