4a1fbb31155e3a10a432b37ad1d92ffb5e6ba57e
Darshan KC <darshanbahadurchhetri@gmail.com>
2024-10-18 20:05:01 +0545

    substring complete

--- substring.sh
+++ substring.sh
@@ -1,12 +1,26 @@
 #! /bin/bash
 
-#Script to extract first 10 characters of a string 
-
 str="Namaste everyone. Let's learn bash script"
 
 echo "Total character in the string is : '${#str}'"
 
-substr="${str:0:10}"  
-  
+#Script to extract first 10 characters of a string 
+
+# substr="${str:0:10}"  
+
+
+# Script to extract remaining characters
+
+substr="${str:10}"
+
+#Script to print 11th character of a String
+
+substr="${str:11:1}"
+
+#Script to extract 11 characters from last
+
+substr="${str:(-11)}"
+
 echo "Substring: $substr"  
 echo "Total characters in Substring: ${#substr} " 
+
