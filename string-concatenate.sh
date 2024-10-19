995b03fdaa50fcebfc0daedfb0efc1120186467e
Darshan KC <darshanbahadurchhetri@gmail.com>
2024-10-19 19:07:15 +0545

    string concatenate added

---
+++ string-concatenate.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+#Script to Concatenate Strings  
+
+#Declaring the first String   
+str1="Hello Namaste."  
+  
+#Declaring the Second String  
+str2=" Let's learn bash." 
+
+#Combining first and second string 
+str3=$str1$str2
+
+#Printing a new string by combining both   
+echo $str3 
