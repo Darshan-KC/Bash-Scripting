5e956a394d88ccc864890996020f225fcf1cfe7b
Darshan KC <darshanbahadurchhetri@gmail.com>
2024-11-08 20:49:10 +0545

    trying some command using alias

--- aliases.sh
+++ aliases.sh
@@ -1,1 +1,16 @@
-#! /bin/bash
+#!/bin/bash
+
+# Enable alias expansion
+shopt -s expand_aliases
+
+# Define aliases
+alias ll='ls -la'
+alias gs='git status'
+
+# Print a message
+echo "Listing files with 'll' alias:"
+ll
+
+# Show the git status using the 'gs' alias
+echo "Displaying Git status using 'gs' alias:"
+gs
